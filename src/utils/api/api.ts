2f10ac2bf0a42700238820dc8bc8d4f1d3bdea93
LucasSSDK <l_ucass_oares@outlook.com>
2023-01-26 18:17:00 -0300

    feat: add axios

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,14 +1,24 @@
-import { LoginRequest } from "../types/request";
+import { LoginRequest } from '../types/request';
+import axios, { AxiosError } from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const api = {
-    login: async ({ email, password}: LoginRequest) => {
-        const response = await fetch('http://localhost:3000/Authorization/login', {
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password})
-        });
-        return await response.json();
+  login: async ({ email, password }: LoginRequest) => {
+    try {
+      const response = await axios.post('/Autorizatio,/login', {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+
+      
+      return response.data;
+    } catch (err) {
+      alert(err);
     }
-}
+  },
+};
