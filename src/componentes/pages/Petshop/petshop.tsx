d3c6cbd110bbf952e4248fd29d960524da527088
LucasSSDK <l_ucass_oares@outlook.com>
2023-01-30 20:52:11 -0300

    up

--- src/componentes/pages/Petshop/petshop.tsx
+++ src/componentes/pages/Petshop/petshop.tsx
@@ -2,22 +2,32 @@ import { useState } from 'react';
 import { Select } from '../../atoms/select/select';
 
 export function Petshop() {
-  const [selectedpet, setPet] = useState<string>('pets');
+  const [selectedPet, setPet] = useState<string>('pets');
+  const [ selectedModule, setSelectModule ] = useState<string>();
 
   const options = ['Pets 1', 'Pets 2', 'Pets 3'];
-  console.log(selectedpet);
+  console.log(selectedPet);
 
   const module = ["Categoria 1", "Categoria 2", "Categoria 3"];
 
   function resultSelect(value: string) {
-    console.log(value);
+    setSelectModule(value);
+
+    console.log(selectedModule);
   }
+
+  const PetsCategoria1 = [ "Papagaio", "Periquito", "Tucano", "Calopsita"]; 
+
+  const PetsCategoria2 = [ "Porquinho da india", "Esquilo", "Preá", "Ramister" ]
+
+  console.log(selectedPet);
 
   return (
     <div>
       <h2>Petshop</h2>
       <Select options={options} selectedOption={setPet} />
       <Select options={module} selectedOption={resultSelect} />
+      
     </div>
   );
 }
